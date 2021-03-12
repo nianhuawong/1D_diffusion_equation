1c8b2bf748db567905ba5f64cc16ce6abe56003f
nianhuawong <nianhuawong@126.com>
2021-03-12 13:34:15 +0800

    显式FTCS格式已完成

--- diffu_solver.h
+++ diffu_solver.h
@@ -1,19 +1,29 @@
 #pragma once
 const double beta = 1.43e-3;
 
+int iter;
 int numberOfGridPoints;
 int numberOfTimeSteps;
+
 double totalTime;
 double dt;
 double ds;
 double sigma;
+
 vector<double> qField;
 vector<double> qField_N1;
 double residual;
-int iter;
+
 vector<double> xCoordinates;
 
 void initialize_parameter();
+void flow_initialization();
+void load_qField();
+void time_marching();
+void boundary_condition();
 void compute_residual();
-vector<double> generate_grid_1D( int numberOfGridPoints);
+void output_residual();
+void output_results();
+
+void generate_grid_1D( int numberOfGridPoints);
 
--- diffusion_equ_solver.cpp
+++ diffusion_equ_solver.cpp
@@ -1,8 +1,37 @@
+//#include "./diffu_solver.h"
 #include<iostream>
 #include<vector>
 #include<fstream>
-#include "diffu_solver.h"
+
 using namespace std;
+
+const double beta = 1.43e-3;
+
+int iter;
+int numberOfGridPoints;
+int numberOfTimeSteps;
+
+double totalTime;
+double dt;
+double ds;
+double sigma;
+
+vector<double> qField;
+vector<double> qField_N1;
+double residual;
+
+vector<double> xCoordinates;
+
+void initialize_parameter();
+void flow_initialization();
+void load_qField();
+void time_marching();
+void boundary_condition();
+void compute_residual();
+void output_residual();
+void output_results();
+
+void generate_grid_1D(int numberOfGridPoints);
 
 int main()
 {
@@ -10,7 +39,7 @@ int main()
 
 	flow_initialization();
 
-	for (int iter = 0; iter < numberOfTimeSteps; ++iter)
+	for (iter = 0; iter < numberOfTimeSteps; ++iter)
 	{
 		load_qField();
 
@@ -25,12 +54,14 @@ int main()
 
 	output_results();
 
+	return 0;
 }
 
 void output_results()
 {
+	cout << "dumping results..." << endl;
 	fstream file;
-	file.open("results.dat", ios_base::out | ios_base::app);
+	file.open("results.dat", ios_base::out );
 
 	file << "TITLE     = \"results\"" << endl;
 	file << "VARIABLES = \"x\", \"qField\"" << endl;
@@ -41,6 +72,8 @@ void output_results()
 	}	
 
 	file.close();
+
+	cout << "done!" << endl;
 }
 
 void output_residual()
@@ -81,15 +114,17 @@ void boundary_condition()
 {
 	qField[0] = 100;
 	qField[numberOfGridPoints - 1] = 0;
+
+	qField_N1[0] = 100;
+	qField_N1[numberOfGridPoints - 1] = 0;
 }
 
 void flow_initialization()
 {
 	//³õÖµ¸³0
 	qField.resize(numberOfGridPoints);
+	qField_N1.resize(numberOfGridPoints);
 	boundary_condition();
-
-	qField_N1 = qField;
 }
 
 void initialize_parameter()
@@ -97,34 +132,39 @@ void initialize_parameter()
 	cout << "Enter number of grid points..." << endl;
 	cin >> numberOfGridPoints;
 	cout << "numberOfGridPoints = " << numberOfGridPoints << endl;
-	
-	cout << "Enter number of time steps..." << endl;
-	cin >> numberOfTimeSteps;
-	cout << "numberOfTimeSteps = " << numberOfTimeSteps << endl;
 
 	cout << "Enter totalTime..." << endl;
 	cin >> totalTime;
 	cout << "totalTime = " << totalTime << endl;
 
-	xCoordinates = generate_grid_1D( numberOfGridPoints );
+
+	generate_grid_1D( numberOfGridPoints );
 	double startCoord = 0.0;
 	double endCoord = 1.0;
 	ds = (endCoord - startCoord) / (numberOfGridPoints - 1);
 
+	int iter_min = 200 * beta / ds / ds;
+
+	cout << "Enter number of time steps..." << "iter > " << iter_min << endl;
+	cin >> numberOfTimeSteps;
+	cout << "numberOfTimeSteps = " << numberOfTimeSteps << endl;
+
 	dt = totalTime / numberOfTimeSteps;
 	sigma = beta * dt / ds / ds;
+	cout << "sigma = " << sigma << endl;
+
 }
 
-vector<double> generate_grid_1D( int numberOfGridPoints )
+void generate_grid_1D( int numberOfGridPoints )
 {
 	double startCoord = 0.0; 
 	double endCoord = 1.0;
 	double ds = ( endCoord - startCoord ) / ( numberOfGridPoints - 1 );
 
-	vector<double> xCoordinates(numberOfGridPoints);
+	xCoordinates.resize(numberOfGridPoints);
 	for (int iNode = 0; iNode < numberOfGridPoints; ++iNode)
 	{
 		xCoordinates[iNode] = startCoord + ds * iNode;
 	}
-	return xCoordinates;
+	//return xCoordinates;
 }
